d2b3369da057e52bb1cd128cb429b5480ae807f0
jimin0427 <cjm0427@hs.ac.kr>
2024-03-09 19:53:08 +0900

    Update Sec03Operator02.java

--- Section03/Sec03Operator02.java
+++ Section03/Sec03Operator02.java
@@ -27,11 +27,27 @@ public class Sec03Operator02 {
         int c = 291;
         double d = 3;
         System.out.println(c / d);
-        System.out.printf("값 "+ c + "를 " + d + "으로 나누기하면 몫은 %f이고, 나머지는 %f이다.", c/d, c%d);
+        System.out.printf("값 "+ c + "를 " + d + "으로 나누기하면 몫은 %f이고, 나머지는 %f이다.\n", c/d, c%d);
     
         /*
-         * 증감 연산자 -> ++, --
-         * 
+         * 증감 연산자 -> ++, -- (단항 연산자 ~> !)
+         *
          */
+        boolean isBatman = true;
+        System.out.println(isBatman); // true
+        System.out.println(!isBatman); // true
+
+        int count = 0;
+        System.out.println("현재 count 값은? " + count++); // 0
+        // 1..?? -> 결과는 0 (주의가 필요)
+        // count 값은 증가된 상태이므로 그냥 아래에서 출력하면 1로 출력
+        // -- 인 경우도 마찬가지
+        System.out.println(count); // 1
+
+        System.out.println("현재 count 값은? " + ++count); // 2
+        System.out.println(count); // 2
+
+        // '현재 카운터 값은 ?' 출력과 동시에 1 증가된 값을 콘솔에 나타나게 하려면?
+        // ++ 증가 연산자를 변수 앞쪽에 선언
     }
 }
