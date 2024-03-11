dbdca23694f6c9bbcd3a96368d9c251c5728596f
jimin0427 <cjm0427@hs.ac.kr>
2024-03-12 01:00:58 +0900

    asd

--- Section04/sec04If01.java
+++ Section04/sec04If01.java
@@ -25,5 +25,49 @@ public class sec04If01 {
             System.out.println("value 값이 : 거짓");
         }
 
+        if(true){
+            System.out.println("value 값이 : 참");
+        }
+
+        /* 
+            ◆ if ~ else if ...
+            여러 개 조건을 비교해서 분기
+            조건을 위에 주냐 아래에 주냐에 따라서 결과가 달라질 수 있으므로 주의
+            먼저 조건에 걸리면 출력하므로 조건을 걸 때 주의
+            마지막 else 구문은 안써도 됨
+        */
+        // Question. 20~30대 까지는 '청년입니다' 출력하고, 40~50대면 '중년입니다.' 60대 이상은 '장년입니다.' 출력하시오
+        int age = 40;
+        if(age >= 20 && age<40){
+            System.out.println("청년입니다");
+        }else if(age >= 40 && age<50){
+            System.out.println("중년입니다.");
+        }else if(age >= 60){
+            System.out.println("장년입니다.");
+        }
+
+        /*
+         * if + boolean 같이 사용하기 
+         * -> 제어문 자체가 불리언 값하고 같이 많이 사용
+         */
+        boolean isSuperman = true;
+        String msg = null;
+
+        if(isSuperman){
+            msg = "슈퍼맨 맞아용";
+        }else{
+            msg = "슈퍼맨 아니에용";
+        }
+
+        System.out.println(msg);
+
+        // 삼항 연산자
+        // -> 특히 프론트엔드 프레임워크에서는 거의 if문 대신으로 많이 사용
+        boolean isWonderWoman = true;
+        String msg2 = null;
+
+        msg2 = isWonderWoman ? "원더우먼 맞아용" : "원더우먼 아니에용";
+
+        System.out.println(msg2);
     }
 }
---
+++ Section04/sec04If02.java
@@ -0,0 +1,34 @@
+package Section04;
+
+public class sec04If02 {
+    public static void main(String[] args) {
+        /*
+         * Switch 조건문
+         * - 변수의 값 또는 어떤 계산식의 결과 값에 따라 조건을 적용하여 분기 처리하는 방식
+         * - 변수의 값?
+         *      - key 라고도 함, key - value 구문
+         *      1. 숫자값 사용 O
+         *      2. 문자값 사용 O
+         *      3. 논리값 사용 X -> boolean -> true, false
+         *      4. enum 타입 사용 O
+         */
+
+        // 기본 틀
+        /*
+         * switch(변수 또는 key){
+         *      case 값 1 : 
+         *          수행할 코드 작성
+         *          break;
+         *      case 값 2: // 해주지 않으면 계속 수행..
+         *          ....
+         *          break;
+         *      case 값 3 :
+         *          ....
+         *          break;
+         *      default :
+         *          어느 조건에도 걸리지 않았을 경우 디폴트 수행
+         *          break;
+         * }
+        */
+    }
+}
