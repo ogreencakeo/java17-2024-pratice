6e4d0ab1a31869ced24e1f160f08d54c8e726711
jimin0427 <cjm0427@hs.ac.kr>
2024-03-13 23:53:55 +0900

    Update Sec05For02.java

--- section05/Sec05For02.java
+++ section05/Sec05For02.java
@@ -2,6 +2,22 @@ package section05;
 
 public class Sec05For02 {
     public static void main(String[] args) {
-        
+        /*
+         * 1. for 반복문 vs while 반복문
+         * - 보통 반복의 횟수를 명확히 알고 있을 때
+         *  -> for, 아니면 while (다 그런 것은 아님)
+         * - 일반적으로 반복을 시작하기 전에 루프 바깥에서 변수가 초기화 되어져 있다면 -> while 문이 좀 더 고려↑
+         * 
+         * 2. 조건에 대해서
+         * - while 문은 조건이 참(true)인 동안 계속 반복을 수행
+         * ~> true를 조건식에 넣어서 많이 사용 (false가 되면 중단)
+         *      ★ 조건이 참인 동안 계속 수행
+         * - for 문은 주어진 (또는 정해진) 조건 동안만 반복을 수행
+         *      ★ 조건 동안만 계속 수행 / 배열과 함께 많이 사용
+         * 
+         * 3. true, false, 1
+         * - 일반적으로 프로그래밍 세계에서는 1 ~> true, 0 ~> false (다 그런것은 아님)
+         * - true 사용 ~> for, while 모두 가능 switch(true)
+        */
     }
 }
