4388b007c17995d11c9a8b7cd8a8bb83bfbf7f28
jimin0427 <cjm0427@hs.ac.kr>
2024-03-14 23:59:30 +0900

    Update Sec05For04.java

--- section05/Sec05For04.java
+++ section05/Sec05For04.java
@@ -3,7 +3,16 @@ package section05;
 public class Sec05For04 {
     public static void main(String[] args) {
         // for 반복문 + break, continue
-        // - break :
-        // - continue : 
+        // - break : 이 키워드를 만나면 반복문이 중단
+        // - continue : 이 키워드를 만나면 반복문이 중단되지 않고, 그 회차만 반복이 중단
+
+        // Question. 1 ~ 20까지 for문으로 출력시 break로 10까지만 출력하시오.
+        // 결과 예) 1 2 3 4 5 6 7 8 9 10
+        for(int i=1; i<=20; i++){
+            System.out.println(i);
+            if(i>10){
+                break;
+            }
+        }
     }
 }
