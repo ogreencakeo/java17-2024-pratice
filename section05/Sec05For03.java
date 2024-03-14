5b335173ea446ebc849d6e824e91af3e8a23f20f
jimin0427 <cjm0427@hs.ac.kr>
2024-03-14 23:16:44 +0900

    Update Sec05For03.java

--- section05/Sec05For03.java
+++ section05/Sec05For03.java
@@ -2,7 +2,7 @@ package section05;
 
 public class Sec05For03 {
     public static void main(String[] args) {
-        // for 반복문 - 생략과 값을 증가시키는 다양한 방법
+        // for 반복문 - 생략, break 반복 탈출, 값을 증가시키는 다양한 방법
         // - 초기식; 조건식; 증감식; -> 생략 가능
         // - 위 3가지 식이 모두 없어도 for 반복문 실행하는 것이 가능함
         //      -> 단, 조건이나 증감이 없다면 무한루프에 빠지게 됨
