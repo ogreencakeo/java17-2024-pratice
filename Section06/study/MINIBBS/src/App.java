da8db3f0e6634b87d752cdf16742933a240e8362
jimin0427 <magente2000@gmail.com>
2024-03-19 03:32:07 +0900

    Update App.java

--- Section06/study/MINIBBS/src/App.java
+++ Section06/study/MINIBBS/src/App.java
@@ -1,11 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
-        start();
+        
+        String greeting = "Hi^^ ";
+
+        new Start().main(greeting);
     }
+    
 
     // public static void start(){
-    public static void start(){
-        System.out.println("=====start=======");
-    }
+    //     System.out.println("=====start=====");
+    // }
+}
+
+class Start{
+    public static void main(String str){ // 파라미터(Parameter), 매개변수
+        System.out.println(str + "~!"); 
+    } // 가능함
 }
