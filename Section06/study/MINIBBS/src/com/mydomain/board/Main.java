304a9722337f9daa19a36a45d50a0712b3849918
jimin0427 <magente2000@gmail.com>
2024-03-19 03:56:28 +0900

    sdf

---
+++ Section06/study/MINIBBS/src/com/mydomain/board/Main.java
@@ -0,0 +1,10 @@
+package com.mydomain.board;
+
+import com.mydomain.board.menu.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== MAIN ===");
+        new Menu().main();
+    }
+}
---
+++ Section06/study/MINIBBS/src/com/mydomain/board/menu/Menu.java
@@ -0,0 +1,9 @@
+package com.mydomain.board.menu;
+
+public class Menu {
+    public void main(){
+        System.out.println("=============");
+        System.out.println("=== MENU ===");
+        System.out.println("=============");
+    }
+}
