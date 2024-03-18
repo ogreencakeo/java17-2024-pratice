36e558e55eda6efe967b00757e4daf4291469b29
jimin0427 <magente2000@gmail.com>
2024-03-19 03:23:51 +0900

    df

---
+++ Section06/study/MINIBBS/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        start();
+    }
+
+    // public static void start(){
+    public static void start(){
+        System.out.println("=====start=======");
+    }
+}
