a57eb689564fcb6012e83b0b6e453dfde45e357d
jimin0427 <cjm0427@hs.ac.kr>
2024-03-19 02:16:38 +0900

    Update Sec05For05.java

--- section05/Sec05For05.java
+++ section05/Sec05For05.java
@@ -44,7 +44,7 @@ public class Sec05For05 {
         /// ...
         for(int i=1; i<=9; i++){
             for(int j=2; j<=9; j++){
-                System.out.printf("%d x %d = %2d  ", j, i, i*j);
+                System.out.printf("%d x %d = %-2d  ", j, i, i*j);
             }
             System.out.println();
         }
