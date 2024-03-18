3a6a3eb29f0b71e5bcc07ab841fbaf7f4c818f85
jimin0427 <magente2000@gmail.com>
2024-03-19 03:59:57 +0900

    sd

--- Section06/study/MINIBBS/src/com/mydomain/board/Main.java
+++ Section06/study/MINIBBS/src/com/mydomain/board/Main.java
@@ -5,6 +5,7 @@ import com.mydomain.board.menu.Menu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== MAIN ===");
-        new Menu().main();
+        // new Menu().main();
+        Menu.main();
     }
 }
--- Section06/study/MINIBBS/src/com/mydomain/board/menu/Menu.java
+++ Section06/study/MINIBBS/src/com/mydomain/board/menu/Menu.java
@@ -1,7 +1,8 @@
 package com.mydomain.board.menu;
 
 public class Menu {
-    public void main(){
+    // public void main(){
+    public static void main(){
         System.out.println("=============");
         System.out.println("=== MENU ===");
         System.out.println("=============");
