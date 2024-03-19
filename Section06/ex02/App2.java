22f68a0e06b8778be3ccb6fbed50f02986936934
jimin0427 <magente2000@gmail.com>
2024-03-20 01:15:28 +0900

    Update App2.java

--- Section06/ex02/App2.java
+++ Section06/ex02/App2.java
@@ -3,16 +3,34 @@ package Section06.ex02;
 public class App2 {
     public static void main(String[] args) {
         String greeting = "Hi ~ ^^";
-        new Start().main(greeting);
+        Start.main(greeting);
     }
 }
 
 class Start{
-    public final double PI = 3.14;
+    public static final double PI = 3.14;
 
-    public void main(String str){
+    public static void main(String str){
         System.out.println("===" + str + "===");
 
         System.out.println(PI);
     }
 }
+
+
+// public class App2 {
+//     public static void main(String[] args) {
+//         String greeting = "Hi ~ ^^";
+//         new Start().main(greeting);
+//     }
+// }
+
+// class Start{
+//     public final double PI = 3.14;
+
+//     public void main(String str){
+//         System.out.println("===" + str + "===");
+
+//         System.out.println(PI);
+//     }
+// } 
