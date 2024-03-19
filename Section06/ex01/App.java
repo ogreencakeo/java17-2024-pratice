56b8fb1a476503c979fecc06af5e9060593a0b99
jimin0427 <magente2000@gmail.com>
2024-03-20 00:29:35 +0900

    Create App.java

---
+++ Section06/ex01/App.java
@@ -0,0 +1,16 @@
+package Section06.ex01;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("== Hi ===");
+        // new Start().main();
+        Start.main();
+    }
+}
+
+class Start{
+    // public void main(){
+    public static void main(){
+        System.out.println("=== Start ===");
+    }
+}
