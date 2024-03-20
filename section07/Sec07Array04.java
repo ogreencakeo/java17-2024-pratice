d81363d3cf31ae74e0afee6dd0e2b092483d33d7
jimin0427 <magente2000@gmail.com>
2024-03-21 02:02:42 +0900

    ds

--- section07/Sec07Array04.java
+++ section07/Sec07Array04.java
@@ -10,23 +10,29 @@ public class Sec07Array04 {
 
         // [1] 1차원 배열
         int stuScore[] = {55, 90, 70};
-        System.out.printf("한 한색의 국/영/수 점수는 = %d, %d, %d", stuScore[0], stuScore[1], stuScore[2]);
-
-        System.out.println();
+        System.out.printf("한 한색의 국/영/수 점수는 = %d, %d, %d \n", stuScore[0], stuScore[1], stuScore[2]);
 
         // [2] 2차원 배열
         // 일단 2차원 배열의 데이터 모습을 상상하면서 머릿속에 그리자
         int stuScore2[][] = {
             {55, 90, 80},
             {75, 40, 50},
-            {50, 50, 90}
+            {50, 50, 90, 50, 60, 40},
+            {50, 50, 90},
         };
 
-        // 출력
+        // 출력 -> 이중 반복문 필요
+        // 행을 반복하는 반복문 : 바깥쪽 for문
+        // 열을 반복하는 반복문 : 안쪽 for문 -> 여기의 조건식을 작성하는 것이 핵심 포인트!
         for(int i=0; i<stuScore2.length; i++){
-            // for(int j=0; j<stuScore2[i].length; j++){
-                System.out.printf("%d번의 국/영/수 성적표 = %d, %d, %d \n", i+1, stuScore2[i][0], stuScore2[i][1], stuScore2[i][2]);
-            // }
+            System.out.print( i + 1 + "번 학생의 국/영/수 성적표 :");
+            for(int j=0; j<stuScore2[i].length; j++){
+                System.out.print(stuScore2[i][j]);
+                if(j != stuScore2[i].length-1){
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
         }
     }
 }
---
+++ section07/Sec07Array05.java
@@ -0,0 +1,15 @@
+package section07;
+
+public class Sec07Array05 {
+    public static void main(String[] args) {
+        // 2차원 배열 + 반복문 이용해서 출력하기 + 총점/평균 구하기
+
+        // 2차원 배열 데이터
+        int stuScore[][] = {
+            {55, 90, 80},
+            {75, 40, 50},
+            {50, 50, 90, 50, 60, 40},
+            {50, 50, 90},
+        };
+    }
+}
