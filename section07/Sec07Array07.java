365f0349da0ce3d337e0a571300e4dea082f7217
jimin0427 <magente2000@gmail.com>
2024-03-21 02:39:18 +0900

    Update Sec07Array07.java

--- section07/Sec07Array07.java
+++ section07/Sec07Array07.java
@@ -1,5 +1,11 @@
 package section07;
 
+// 9
+// arr[2] 출력 => 9
+// change2 메서드 호출 후 결과는 ? => 100
+// arr 출력 => 30
+// change1 메서드 호출 후 결과는 ? => 100
+
 public class Sec07Array07 {
     public static void main(String[] args) {
         int nums[][] = new int[][]{
@@ -8,23 +14,23 @@ public class Sec07Array07 {
             {7, 8, 9},
         };
 
-        System.out.println(nums[2][2]); // 9 
+        System.out.println(nums[2][2]); 
 
         change2(nums[2]);
-        System.out.println("change2 메서드 호출 후 결과는 ? => " + nums[2][2]); // 100
+        System.out.println("change2 메서드 호출 후 결과는 ? => " + nums[2][2]); 
         
         change1(nums[2][2]);
-        System.out.println("change1 메서드 호출 후 결과는 ? => " + nums[2][2]); // 30? 100?
+        System.out.println("change1 메서드 호출 후 결과는 ? => " + nums[2][2]); 
 
     }
 
     public static void change1(int arr){
         arr = 30;
-        System.out.println("arr 출력 => " + arr); // 30
+        System.out.println("arr 출력 => " + arr); 
     }
 
     public static void change2(int arr[]){
-        System.out.println("arr[2] 출력 => " + arr[2]); // 7?8?9?
+        System.out.println("arr[2] 출력 => " + arr[2]); 
         arr[2] = 100;
     }
 }
