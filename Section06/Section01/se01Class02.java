86a3869dd729ae4df6b29f1a0d83556c95f6b9eb
jimin0427 <magente2000@gmail.com>
2024-03-21 00:21:52 +0900

    dsf

---
+++ Section06/Section01/se01Class02.java
@@ -0,0 +1,9 @@
+package Section06.Section01;
+
+public class se01Class02 {
+    public static void main(String[] args) {
+        // 가장 기본적인 형태의 Person 클래스를 만들어보시오.
+        // 지금 여기는 프로그램 진입점 역할을 하는 main 메서드안이라는 것을 까먹지 말자.
+        
+    }
+}
