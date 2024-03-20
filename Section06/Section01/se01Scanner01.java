e84523dfb1b862012e2af6b8c9690a19e7698ad7
jimin0427 <magente2000@gmail.com>
2024-03-20 23:25:55 +0900

    Create se01Scanner01.java

---
+++ Section06/Section01/se01Scanner01.java
@@ -0,0 +1,12 @@
+package Section06.Section01;
+
+public class se01Scanner01 {
+    public static void main(String[] args) {
+        // 사용자 입력 : Scanner 클래스
+        // - 공부하는 유저 입장에서 본다면 출력만 하면 지루한데 입력도 같이 하면 재밌음
+        // - 자바에서 사용자 입력은 Scanner 클래스를 사용하는게 가장 편한 방법이나 다른 방법들도 있다는 것은 기억
+        // - import java.util.Scanner; (임포트는 자동으로 삽입, 패키지와 클래스 사이)
+        // - 전체적으로 사용법 자체는 어렵지 않으나 그래도 주의할 부분은 있다.
+        // - 객체 변수명 : s, sc, scan, scanner
+    }
+}
