3ce7db841117ac8bc112a919c4dfa17afb1f2192
jimin0427 <magente2000@gmail.com>
2024-03-20 23:59:40 +0900

    Update se01Scanner01.java

--- Section06/Section01/se01Scanner01.java
+++ Section06/Section01/se01Scanner01.java
@@ -20,30 +20,46 @@ public class se01Scanner01 {
         // [1]
         // 사용자 입력을 통해서 이름(문자열), 나이(정수), 몸무게(실수)를 받아서 출력하시오.
         // 객체 생성
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        // 입력받는 정보등을 저장할 변수 선언
-        String name;
-        int age;
-        double weight;
+        // // 입력받는 정보등을 저장할 변수 선언
+        // String name;
+        // int age;
+        // double weight;
 
-        // 사용자 입력
-        System.out.print("이름을 입력하시오 : ");
-        name = sc.nextLine();
+        // // 사용자 입력
+        // System.out.print("이름을 입력하시오 : ");
+        // name = sc.nextLine();
 
-        System.out.print("나이를 입력하시오 : ");
-        age = sc.nextInt();
+        // System.out.print("나이를 입력하시오 : ");
+        // age = sc.nextInt();
 
-        System.out.print("몸무게를 입력하시오 : ");
-        weight = sc.nextDouble();
+        // System.out.print("몸무게를 입력하시오 : ");
+        // weight = sc.nextDouble();
+
+        // // 출력
+        // System.out.println("-------------------");
+        // System.out.printf("회원 이름 : %s\n", name);
+        // System.out.printf("회원 나이 : %d살\n", age);
+        // System.out.printf("회원 몸무게 : %fkg\n", weight);
+
+        // [2]
+        // Scanner 클래스로 사용자 입력 처리시 주의사항
+        // 객체 생성
+        Scanner sc2 = new Scanner(System.in);
+
+        // 이상이 없는 경우
+        System.out.println("나이와 이름을 입력하시오 : ");
+        int age2 = sc2.nextInt();
+        sc2.nextLine();
+        String name2 = sc2.nextLine();
 
         // 출력
-        System.out.println("-------------------");
-        System.out.printf("회원 이름 : %s\n", name);
-        System.out.printf("회원 나이 : %d살\n", age);
-        System.out.printf("회원 몸무게 : %fkg\n", weight);
+        System.out.printf("제 이름은 %s이고, 나이는 %d입니다. \n", name2, age2);
+
 
         // 객체 종료
-        sc.close();
+        // sc.close();
+        sc2.close();
     }  
 }
