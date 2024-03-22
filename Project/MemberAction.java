b10aa62341ae1126067dcdc1b29a2355e6399e70
jimin0427 <magente2000@gmail.com>
2024-03-23 02:06:40 +0900

    Update MemberAction.java

--- Project/MemberAction.java
+++ Project/MemberAction.java
@@ -8,7 +8,12 @@ public class MemberAction {
     public void memberInsert(Scanner sc){
         System.out.print("이름 >> ");
         String name = sc.next();
-        System.out.print(name);
+        System.out.print("전화 >> ");
+        String hp = sc.next();
+        System.out.print("이메일 >> ");
+        String email = sc.next();
+        System.out.printf("회원정보 : [이름 : %s, 전화번호 : %s, 메일 : %s]\n",
+                            name, hp, email);
     }
 
     public void memberInsertOk(){
