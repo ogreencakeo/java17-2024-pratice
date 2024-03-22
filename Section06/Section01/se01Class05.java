c1897cdfdc2985e8c77050510918837d585a4871
jimin0427 <magente2000@gmail.com>
2024-03-23 04:22:19 +0900

    Update se01Class05.java

--- Section06/Section01/se01Class05.java
+++ Section06/Section01/se01Class05.java
@@ -16,14 +16,20 @@ public class se01Class05 {
 
 class Swap{
     private void swap(int x, int y){
+        // 전달되는 값이 기본형인 경우 -> 값 자체가 복사되어 전달
+        // 전달되는 값이 참조형인 경우 -> 참조하는 값(주소, 번지)이 복사되어 전달
+        int temp = x;
+        x = y;
+        y = temp;
         System.out.println("SWAP - private");
-        System.out.println("x + y = " + (x+y));
+        System.out.println("바꾸기 완료후 x = " + x);
+        System.out.println("바꾸기 완료후 y = " + y);
     }
 
     public void swapTest(int x, int y){
         System.out.println("x = " + x);
         System.out.println("y = " + y);
 
-        swap(x, y);
+        swap(x, y); 
     }
 }
