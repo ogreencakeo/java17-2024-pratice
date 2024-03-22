4ad48064982ae9f8f98c640d207eeb8ffb454222
jimin0427 <magente2000@gmail.com>
2024-03-23 03:44:16 +0900

    Create se01Class05.java

---
+++ Section06/Section01/se01Class05.java
@@ -0,0 +1,9 @@
+package Section06.Section01;
+
+public class se01Class05 {
+    public static void main(String[] args) {
+        // Call By Value - 값에 의한 호출
+        // - 자바에서 메서드에 인자(인수, 전달값)를 전달하는 방식중 하나
+        
+    }
+}
