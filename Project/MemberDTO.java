35896a9c38e0ebeba499ea917cdb21420deca1f3
jimin0427 <magente2000@gmail.com>
2024-03-24 03:15:00 +0900

    sd

--- Project/MemberDTO.java
+++ Project/MemberDTO.java
@@ -1,7 +1,24 @@
 package Project;
 
 // 클래스 사이에서 회원 정보를 주고 받을 때 사용하는 회원 객체 한 명에 대한 정보를 담는 클래스
+// - 메서드와 메서드 사이에서 회원 정보를 주고 받을 때 사용
+// - DTO(Data Transter Object)
+// - DTO는 로직을 가지지 않는 순수한 데이터 객체
+// - 이외에도 VO, DAO(DB 연동 관련) 등..
+
 
 public class MemberDTO {
+    // Field
+    private int no;
+    private String name;
+    private String hp;
+    String eml;
     
+    // Constructor
+
+    // Setter Method
+    
+    // Getter Method 
+
+    // toString Method
 }
