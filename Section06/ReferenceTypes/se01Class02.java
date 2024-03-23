d81a5735e71f764e1e0734a07b440635c4b40655
jimin0427 <magente2000@gmail.com>
2024-03-24 02:36:06 +0900

    Update se01Class02.java

--- Section06/ReferenceTypes/se01Class02.java
+++ Section06/ReferenceTypes/se01Class02.java
@@ -1,11 +1,15 @@
 package Section06.ReferenceTypes;
-
+// 3
+// arr 출력 => 3
+// arr 출력 => 100
+// change2 메서드 호출 후 결과는 ? => 3
+// arr[2] 출력 => 30
+// change1 메서드 호출 후 결과는 ? => 30
 public class se01Class02 {
     public static void main(String[] args) {
         // 참조 타입의 전달 - 배열 객체
         int nums[] = new int[]{1, 2, 3, 4, 5};
-        System.out.println(nums[2]); // 3
-        System.out.println();
+        System.out.println(nums[2]);
 
         change2(nums[2]);
         System.out.println("change2 메서드 호출 후 결과는 ? => " + nums[2]); //
@@ -17,10 +21,13 @@ public class se01Class02 {
     }
 
     public static void change1(int[] arr){
-
+        arr[2] = 30;
+        System.out.println("arr[2] 출력 => " + arr[2]); 
     }
 
-    public static void change2(int x){
-
+    public static void change2(int arr){
+        System.out.println("arr 출력 => " + arr);
+        arr = 100;
+        System.out.println("arr 출력 => " + arr);
     }
 }
