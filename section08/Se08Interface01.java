3ac2b5221bd08ad2b1ffdabc31b7fecae096db0a
jimin0427 <magente2000@gmail.com>
2024-03-27 00:07:28 +0900

    Update Se08Interface01.java

--- section08/Se08Interface01.java
+++ section08/Se08Interface01.java
@@ -1,14 +1,37 @@
 package section08;
 
 public class Se08Interface01 {
-    
+    public static void main(String[] args) {
+        // 인터페이스(Interface)
+
+        // Hi, Welcome 2개의 인터페이스를 구현한 클래스 사용 ~> 이 클래스의 객체를 생성해서 사용하겠다라는 의미
+        Greeting g1 = new Greeting();
+        g1.hi();
+        g1.welcome("홍길동");
+    }    
 }
 
-// 인터페이스 정의
-// 클래스 정의하듯이 하면 됨 -> 그저 class 대신 interface 써주면 되는구나 생각하면 쉬움
+// 인터페이스 정의 ~> 클래스 정의하듯이 ~> class 키워드 대신 interface 키워드 사용
 interface Hi{
-    public static final int MAX_SCORE = 100; // 성수
-    public abstract void hi(); // 추상 메서드
-    // 이 둘만 멤버가 될 수 있음
-    // public static final, public abstract 수식자들은 생략 가능
+    public static final int MAX_SCORE = 100; // 상수
+    public abstract void hi();      // 추상 메서드 
+}
+
+// 수식자 생략하고 한 줄로 추상 메서드 정의
+interface Welcome{
+    void welcome(String name);
+}
+
+// 인터페이스 구현 클래스
+class Greeting implements Hi, Welcome{
+    
+    @Override
+    public void hi(){
+        System.out.println("안녕하세요~");
+    }
+
+    @Override
+    public void welcome(String name){
+        System.out.printf("%s 화원남 환영합니다. 최고 점수 획득은 %d점 입니다.\n", name, MAX_SCORE);
+    }
 }
