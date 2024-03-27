0591568352316e8f1baec9ca12cd9451d7fcb7b3
jimin0427 <magente2000@gmail.com>
2024-03-28 03:24:36 +0900

    Create Se08Inheritance04.java

---
+++ Section08/Se08Inheritance04.java
@@ -0,0 +1,28 @@
+package Section08;
+
+public class Se08Inheritance04 {
+    public static void main(String[] args) {
+        // 클래스간의 관계 : Has-a 관계를 이용한 클래스 만들기 - 배열편
+
+    }
+}
+
+class TPerson{
+    // Field
+    public int id;       // 번호
+    public String name;  // 이름
+    public int nums;     // 시험 과목 수
+    Score socre[];       // 스코어 배열
+
+    //Constructor
+
+    // Method
+}
+
+class Score{
+    // Field
+
+    //Constructor
+
+    // Method
+}
