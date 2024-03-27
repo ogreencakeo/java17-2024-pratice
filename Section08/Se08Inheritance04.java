e289342601d4082e7604421556b65d14d4d9041e
jimin0427 <magente2000@gmail.com>
2024-03-28 03:59:50 +0900

    Update Se08Inheritance04.java

--- Section08/Se08Inheritance04.java
+++ Section08/Se08Inheritance04.java
@@ -4,6 +4,33 @@ public class Se08Inheritance04 {
     public static void main(String[] args) {
         // 클래스간의 관계 : Has-a 관계를 이용한 클래스 만들기 - 배열편
 
+        // p1 객체 생성
+        TPerson tperson1 = new TPerson(001, "김철수", 3);
+        
+        // p1 객체 과목이름, 점수 입력
+        // 테스트
+        System.out.println(tperson1.score[0].getScore()); //  null => 0점 ]
+
+        tperson1.score[0].setScore("java", 80);
+        tperson1.score[1].setScore("python", 90);
+        tperson1.score[2].setScore("react", 100);
+
+        // System.out.println(tperson1.score[0].getScore());
+        // System.out.println(tperson1.score[1].getScore()); 
+        // System.out.println(tperson1.score[2].getScore());
+
+        tperson1.printInfo();
+
+        // p2 객체 생성
+        TPerson p2 = new TPerson(002, "김장미", 5);
+        p2.score[0].setScore("c언어", 95);
+        p2.score[1].setScore("c++", 85);
+        p2.score[2].setScore("javascript", 100);
+        p2.score[3].setScore("jsp", 75);
+        p2.score[4].setScore("spring", 80);
+
+        p2.printInfo();
+        
     }
 }
 
@@ -28,6 +55,19 @@ class TPerson{
     }
 
     // Method
+    public void printInfo(){
+        System.out.println("-------------------------------");
+        System.out.printf(">> 이름 : %s, 아이디 %d \n", this.name, this.id);
+        // 향상된 for문
+        for(Score s : score){
+            System.out.println(s.getScore());
+        }
+        // for문
+        // for(int i=0; i<score.length; i++){
+        //     System.out.println(score[i].getScore());
+        // }
+        System.out.println("-------------------------------");
+    }
 
 }
 
