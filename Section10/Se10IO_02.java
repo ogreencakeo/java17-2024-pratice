0e53d4db905c8356a5d2fb1950612a29f5b2c3ff
jimin0427 <magente2000@gmail.com>
2024-03-28 04:59:07 +0900

    Update Se10IO_02.java

--- Section10/Se10IO_02.java
+++ Section10/Se10IO_02.java
@@ -4,6 +4,7 @@ public class Se10IO_02 {
     public static void main(String[] args) {
         // 자바 입출력
         // - 파일 용량 체크하기 ~> File 클래스, file.length() 메서드 사용
+        // - file.exists() 파일 존재 여부 체크 => 파일이 존재하지 않으면 반환 값은 => 0
         
     }
 }
