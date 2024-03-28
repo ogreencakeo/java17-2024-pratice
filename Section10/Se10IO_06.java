c0579f9c9b12224cbffd6f12cd1cfcecf40ec9d6
jimin0427 <magente2000@gmail.com>
2024-03-29 03:13:57 +0900

    Section10\Se10IO_06

---
+++ Section10/Se10IO_06.java
@@ -0,0 +1,12 @@
+package Section10;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class Se10IO_06 {
+    public static void main(String[] args) {
+        // 자바 입출력
+        // BufferedWriter 클래스를 이용하여 출력하기
+        // BufferedWriter(new FileWriter())
+    }
+}
