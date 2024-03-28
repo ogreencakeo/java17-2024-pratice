e8edcb23e25e1661d588f3450ae296ac0d8157e7
jimin0427 <magente2000@gmail.com>
2024-03-29 03:02:40 +0900

    Se10IO_05

--- Section10/Se10IO_05.java
+++ Section10/Se10IO_05.java
@@ -3,9 +3,10 @@ package Section10;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class Se10IO_05 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         // 자바 입출력
         // - 문자열 배열에 들어있는 한글 내용을 텍스트 파일에 기록해보시오.
         // - 사용 클래스 : PrintWriter
@@ -16,7 +17,7 @@ public class Se10IO_05 {
         // 파일 객체 생성
         File file = new File(filePath);
 
-        try (PrintWriter pw = new PrintWriter(file)) {
+        try (PrintWriter pw = new PrintWriter(file, "UTF-8")) {
             // 배열
             String animals[] = new String[]{"Dog", "Cat", "코끼리", "토끼"};
             for(String s : animals){
