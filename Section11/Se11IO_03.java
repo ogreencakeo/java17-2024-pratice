2d1a1195052a9ede739dea6bec7045fd0bf9fbaf
jimin0427 <magente2000@gmail.com>
2024-03-31 02:49:34 +0900

    Create Se11IO_03.java

---
+++ Section11/Se11IO_03.java
@@ -0,0 +1,13 @@
+package Section11;
+
+public class Se11IO_03 {
+    public static void main(String[] args) {
+        // 파일 복사하기
+        // - 프로젝트 루트 경로에 있는 sample.txt 파일을 루트내 ztemp 폴더로 복사해보시오.
+
+        // - 어떤 클래스를 사용할까?
+        // - 텍스트 파일 외에도 이미지 파일도 복사가 가능할까?
+
+        
+    }
+}
