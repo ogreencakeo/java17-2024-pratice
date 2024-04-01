2d62ba134184d56f9ba66ce5a535068e2c375774
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-02 05:18:52 +0900

    Create Se11String_01.java

---
+++ Section11/Se11String_01.java
@@ -0,0 +1,23 @@
+package Section11;
+
+// String 클래스 자주 사용하는 메서드
+        // - length()           int         글자 수 반환
+        // - getBytes()         byte[]      바이트 배열로 반환
+        // - equals()           boolean
+        // - charAt()           char        charAt(int index) 특정 인덱스의 문자를 반환
+        // - indexOf()          int         문자열의 위치 (인덱스)를 반환
+        // - valueOf()          String      기본형 타입의 값을 문자열로 변환 ~> valueOf(true) // "true"
+        // - trim()                         공백 제거
+        // - toUpperCase()                  대문자 ~> 새 문자열을 리턴
+        // - toLowerCase()                  소문자
+        // - replace()
+        // - substring()
+        // - concat()
+        // - join()
+        // - split()
+
+public class Se11String_01 {
+    public static void main(String[] args) {
+        
+    }
+}
