6671e109ce5591c9c39e409a1d19e842bb18024b
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-04 03:43:09 +0900

    Create Se12Generic06.java

---
+++ Setion12/Se12Generic06.java
@@ -0,0 +1,17 @@
+package Setion12;
+
+public class Se12Generic06 {
+    public static void main(String[] args) {
+        // 제네릭과 배열리스트(ArrayList) 함께 사용하기
+        // - 가장 많이 사용되는 것 중 하나이므로 속달될 때까지 반복해서 연습
+        // - 앞서 제네릭 관련 문법을 배웠으면, 이제 ArrayList 클래스도 기본은 볼 수 있게 되었다.
+        // - 보다 자세한 ArrayList 학습 및 예제는 추후 컬렉션 파트에서 자세히 공부
+
+        // - 조류 객체만 담는 클래스를 만들어보시오.
+        //      1. Birds 클래스를 추상 클래스로 만들고, 추상 메서드로 cry() 메서드를 정의한다.
+        //      2. Birds 클래스를 상속 받는 Eagle 자식 클래스를 만들고, Cry() 메서드를 구체적으로 구현한다. 
+        //      3. Birds 클래스를 상속 받는 Owl 자식 클래스를 만들고, Cry() 메서드를 구체적으로 구현한다. 
+        //      4. Dog 클래스를 만들고, Birds 클래스는 상속 받지 않는다.
+        //      5. BirdsList 클래스를 제네릭으로 만들고, Eagle, Owl 객체를 ArrayList에 저장한다.
+    }
+}
