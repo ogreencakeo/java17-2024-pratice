6ec45fb232b2e60ed8b0eadab1070da00b18a3af
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-08 05:21:22 +0900

    Create Se13Collection010.java

---
+++ Section13/Se13Collection010.java
@@ -0,0 +1,19 @@
+package Section13;
+
+public class Se13Collection010 {
+    public static void main(String[] args) {
+        // 컬렉션 프레임 워크 - 링크드리스트, 스택, 큐
+        testLinkdeList();
+        System.out.println("---------------------------링크드리스트");
+
+        // 백터의 경우는 나중에 사용하고자 할 때 들여다보면 되고, 배열리스트와 거의 같으므로 이것을 사용하면 됨
+        // 한가지 기억할 것은 Stack 클래스는 Vector 클래스를 상속받아 만들어진 것
+
+        System.out.println("---------------------------스텍");
+        System.out.println("---------------------------큐");
+    }
+
+    public static void testLinkdeList(){
+        
+    }
+}
