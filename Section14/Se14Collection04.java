c620703dcbda83a1108e5b68ef682a77e69a18a8
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-11 18:57:55 +0900

    Update Se14Collection04.java

--- Section14/Se14Collection04.java
+++ Section14/Se14Collection04.java
@@ -1,6 +1,7 @@
 package Section14;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Se14Collection04 {
     public static void main(String[] args) {
@@ -39,9 +40,28 @@ public class Se14Collection04 {
         System.out.println("현재 사이즈 : " + set.size()); // 3
         
         set.add(new OPerson("Superman"));
-        System.out.println("----------------------------");
+        System.out.println("------------------------------");
         System.out.println(set);
         System.out.println("현재 사이즈 : " + set.size()); // 3 // 재정의 전 : 4
+        
+        // 요소 찾기 (검색) => hascode() 재정의, equals() 재정의 살펴보기
+        // get() 메서드 사용 불가 => set은 순서가 없기 때문
+        // 대신 contains 사용 가능
+        // System.out.println(set.contains("Superman")); // false
+        System.out.println(set.contains(new OPerson("Superman"))); // true
+
+        // 전체 요소 출력
+        // [1] : iterator() 사용
+        Iterator<OPerson> iter = set.iterator();
+        while(iter.hasNext()){
+            OPerson el = iter.next();
+            System.out.println(el);
+        }
+        
+        System.out.println("------------------------------");
+
+        // [2] : forEach() + 람다식
+        set.forEach(ele -> System.out.println(ele));
     }
 }
 
@@ -57,6 +77,7 @@ class OPerson{
 
     // name 필드를 기반으로 해시 코드를 생성하여 반환하고 있다. 
     // 이는 같은 이름을 갖는 OPerson 객체들은 같은 해시 코드를 갖게 된다.
+    // 해시 테이블에서 쉽게 그룹화하고 검색할 수 있다.
     @Override
     public int hashCode(){
         return name.hashCode();
