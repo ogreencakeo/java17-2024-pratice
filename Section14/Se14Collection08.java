dc4b4f0bf70322c3139ebd18454d996677241b6e
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-16 23:13:36 +0900

    Update Se14Collection08.java

--- Section14/Se14Collection08.java
+++ Section14/Se14Collection08.java
@@ -1,5 +1,6 @@
 package Section14;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Se14Collection08 {
@@ -27,5 +28,27 @@ public class Se14Collection08 {
         // [1] 모든 과일명과 가격을 전체 출력하시오.
         // 전체 출력 1
         System.out.println("전체 출력 : " + map);
+
+        // 전체 출력 2 ~> 중요! ~> 향상된 for 반복문
+        for(Map.Entry<String, Integer> el : map.entrySet()){
+            // String key = el.getKey();
+            // Integer value = el.getValue();
+            // System.out.println(key + " : " + value);
+            // System.out.println(el);
+
+            System.out.print(el.getKey() + " : " + el.getValue() + ", ");
+        }
+
+        // Map.Entry 객체
+        // 1. map.entrySet() 메서드를 사용하여 트리맵의 모든 엔트리를 Map.Entry 객체로 변환
+        // 2. Map.Entry 객체는 키와 값을 저장하는 두 개의 속성을 가지고 있는데 -> getKey(), getValue()
+        
+        // 전체 출력 3 ~> 중요! ~> 향상된 for 반복문 ~> map.keySet()
+        System.out.println("\n-----------------------------------------------");
+        for(String key : map.keySet()){
+            // System.out.print(key + ", ");
+            System.out.print(key + " : " + map.get(key) + ", ");
+        }
+
     }
 }
