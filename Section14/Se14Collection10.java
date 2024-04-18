8c3a24eab06eecce7358d4f4ede362df492fb4f6
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-18 22:29:41 +0900

    Update Se14Collection10.java

--- Section14/Se14Collection10.java
+++ Section14/Se14Collection10.java
@@ -1,5 +1,6 @@
 package Section14;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -24,6 +25,27 @@ public class Se14Collection10 {
         System.out.println("------------------------------------");
 
         // 키를 내림차순 정렬하여 출력하시오
-        
+        // TreeMap 객체 생성 : 이때, 구현한 FruitsComparator 를 인자로 전달하면서 생성
+        TreeMap<Integer, String> tmap2 = new TreeMap<>(new FruitsComparator()){{
+            put(10000, "참외");
+            put(20000, "포도");
+            put(30000, "사과");
+            put(50000, "체리");
+            put(40000, "수박");
+        }};
+
+        // 내림차순 정렬
+        for(Map.Entry<Integer, String> desc : tmap2.entrySet()){
+            System.out.println(desc);
+        } 
+
+    }
+}
+
+// Comparator 인터페이스 구현
+class FruitsComparator implements Comparator<Integer>{
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
     }
 }
