d3e64fbfbb2c168e6d1da786e1e455d28402a9bf
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-18 22:32:12 +0900

    Create Se14Collection11.java

---
+++ Section14/Se14Collection11.java
@@ -0,0 +1,11 @@
+package Section14;
+
+public class Se14Collection11 {
+    public static void main(String[] args) {
+        // 컬렉션 프레임워크 - Comparable 인터페이스를 구현하여 Person 클래스 객체 정렬하기
+    }
+}
+
+class UPerson{
+    
+}
