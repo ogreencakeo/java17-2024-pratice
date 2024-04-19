1693ef7f516dc347d4221c3e09b35e735a0c7276
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-19 21:21:31 +0900

    Update NullPointerEx.java

--- Section15/NullPointerEx.java
+++ Section15/NullPointerEx.java
@@ -5,7 +5,11 @@ public class NullPointerEx {
         
         String str = null;
         // 이 예외는 객체(인스턴스)가 없는데 어떻게 인스턴스에 있는 toString() 호출하니?
-        System.out.println(str.toString());
+        try {
+            System.out.println(str.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
