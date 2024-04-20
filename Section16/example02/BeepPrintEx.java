7bd054f405eef205a3c86848fe47909fad61d102
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-20 23:49:57 +0900

    Create BeepPrintEx.java

---
+++ Section16/example02/BeepPrintEx.java
@@ -0,0 +1,57 @@
+package Section16.example02;
+
+// java.awt.Toolkit;
+
+public class BeepPrintEx {
+    // main() 은 JVM이 실행한다.
+    public static void main(String[] args) {
+
+        // 직접 생성하는 멀티 스레드 방법 3가지
+        // 방법 1 : 구현 객체 대입
+        // Runnable runnable = new Beep(); // 구현 객체 대입
+        // Thread thread = new Thread(runnable);
+        // thread.start();
+
+        // 방법 2 : 익명구현 객체
+        // Thread thread = new Thread(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         Toolkit toolkit = Toolkit.getDefaultToolkit();
+        //         for(int i=0; i<5; i++){
+        //             // toolkit.beep();
+        //             try {
+        //                 System.out.println("비프음 실행하는 스레드 명 : " + Thread.currentThread().getName());
+        //                 Thread.sleep(500);
+        //             } catch (InterruptedException e) {
+        //                 e.printStackTrace();
+        //             }
+        //         }
+        //     }
+        // });
+        // thread.start();
+
+        // 방법 3 -> 람다식 (함수적 인터페이스) JDK1.8 코드 절약, 가독성
+        Thread thread = new Thread(() -> {
+            // Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for(int i=0; i<5; i++){
+                // toolkit.beep();
+                try {
+                    System.out.println("비프음 실행하는 스레드 명 : " + Thread.currentThread().getName());
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+        for(int i=0; i<5; i++){
+            System.out.println("삐융");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {}
+        }
+        
+        // 싱글 스레드를 멀티 스레드로 바꿔서 프로그램을 만들어야 한다.
+    }    
+}
