8c922f72c5addfd245e50fd50e566180dcce0bc4
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-20 23:49:52 +0900

    Create Beep.java

---
+++ Section16/example02/Beep.java
@@ -0,0 +1,24 @@
+package Section16.example02;
+// java.awt.Toolkit;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+// Runnable 인터페이스를 직접 구현하고 있다.
+public class Beep implements Runnable{
+
+    @Override
+    public void run() {
+        // Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i=0; i<5; i++){
+            // toolkit.beep();
+            try {
+                System.out.println("비프음 실행하는 스레드 명 : " + Thread.currentThread().getName());
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
