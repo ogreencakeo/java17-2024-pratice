e96077f5373b88f7a5d55225ad31362f5c0d0412
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-22 02:38:05 +0900

    Create ThreadA.java

---
+++ Section16/example05/ThreadA.java
@@ -0,0 +1,14 @@
+package Section16.example05;
+
+public class ThreadA extends Thread{
+    ThreadA(){
+        this.setName("ThreadA"); // 스레드 이름을 지정하고 있다.
+    }
+    @Override
+    public void run() {
+        System.out.println("[ 현재 실행 중인 스레드 ] : " + this.getName());
+        for(int i=0; i<5; i++){
+            System.out.println(this.getName() + "가 출력한 내용 : " + i);
+        }
+    }   
+}
