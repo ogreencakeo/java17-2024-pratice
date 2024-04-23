73db3ff7cab5c4723ae44b0091b50fc432897ec2
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-23 20:17:19 +0900

    Create User1.java

---
+++ Section16/example07/User1.java
@@ -0,0 +1,18 @@
+package Section16.example07;
+
+public class User1  extends Thread{
+    // 공유객체 선언
+    private Calculator calculator;
+
+    public void setCalculator(Calculator calculator){
+        this.calculator = calculator;
+        this.setName("User-1"); // 스레드 이름 지정
+    }
+
+    @Override
+    public void run() {
+        this.calculator.print();
+        // 공유객체의 필드인 memory 값에 100을 변경
+        this.calculator.setMemory(100);
+    }
+}
