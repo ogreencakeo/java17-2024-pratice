ec7dde400f9a9aaa20e5423f372aaaa2a8e1a3c5
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-04-30 02:35:34 +0900

    Create ThreadB.java

---
+++ Section16/example10/ThreadB.java
@@ -0,0 +1,22 @@
+package Section16.example10;
+
+public class ThreadB extends Thread{
+    public boolean stop = false;
+    public boolean work = true;
+
+    @Override
+    public void run() {
+        while(!stop){
+            if(work){
+                try {
+                    Thread.sleep(50);
+                } catch (Exception e) {}
+                System.out.println("ThreadB의 작업내용");
+            }else{
+                // 무의미한 반복을 막기 위해 다른 스레드한테 양보함.
+                Thread.yield();
+            }
+        }
+        System.out.println("ThreadB 종료");
+    }
+}
